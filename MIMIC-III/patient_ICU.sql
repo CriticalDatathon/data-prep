f1a46d5861506fe2953585fa70631d9d06047065
Maria Loureiro <59094160+M4riaLoureiro@users.noreply.github.com>
2023-02-18 22:00:52 +0000

    create race_group matching criteria used in MIMIC-IV

--- MIMIC-III/patient_ICU.sql
+++ MIMIC-III/patient_ICU.sql
@@ -10,21 +10,65 @@ icu.subject_id
 , icu.admission_age
 , icu.first_hosp_stay
 , icu.first_icu_stay
-, icu.icustay_seq
 , icu.admittime
 , icu.dischtime
 , icu.intime as icu_intime
 , icu.outtime as icu_outtime
 , icu.los_hospital
 , icu.los_icu
-, icu.ethnicity
-, icu.ethnicity_grouped
 , icu.icustay_seq
 , sf.sofa
 , ad.language
 , heightweight.weight_first as weight
 , heightweight.height_first as height
 , heightweight.weight_first / (POWER(heightweight.height_first/100, 2)) AS BMI
+, icu.ethnicity
+, CASE WHEN icu.ethnicity IN
+  (
+       'WHITE' --  40996
+     , 'WHITE - RUSSIAN' --    164
+     , 'WHITE - OTHER EUROPEAN' --     81
+     , 'WHITE - BRAZILIAN' --     59
+     , 'WHITE - EASTERN EUROPEAN' --     25
+     , 'PORTUGUESE' --     61
+  ) THEN 'White'
+  WHEN icu.ethnicity IN
+  (
+      'BLACK/AFRICAN AMERICAN' --   5440
+    , 'BLACK/CAPE VERDEAN' --    200
+    , 'BLACK/HAITIAN' --    101
+    , 'BLACK/AFRICAN' --     44
+    , 'CARIBBEAN ISLAND' --      9
+  ) THEN 'Black'
+  WHEN icu.ethnicity IN
+    (
+      'HISPANIC OR LATINO' --   1696
+    , 'HISPANIC/LATINO - PUERTO RICAN' --    232
+    , 'HISPANIC/LATINO - DOMINICAN' --     78
+    , 'HISPANIC/LATINO - GUATEMALAN' --     40
+    , 'HISPANIC/LATINO - CUBAN' --     24
+    , 'HISPANIC/LATINO - SALVADORAN' --     19
+    , 'HISPANIC/LATINO - CENTRAL AMERICAN (OTHER)' --     13
+    , 'HISPANIC/LATINO - MEXICAN' --     13
+    , 'HISPANIC/LATINO - COLOMBIAN' --      9
+    , 'HISPANIC/LATINO - HONDURAN' --      4
+    , 'SOUTH AMERICAN' --      8
+  ) THEN 'Hispanic'
+  WHEN icu.ethnicity IN
+  (
+      'ASIAN' --   1509
+    , 'ASIAN - CHINESE' --    277
+    , 'ASIAN - ASIAN INDIAN' --     85
+    , 'ASIAN - VIETNAMESE' --     53
+    , 'ASIAN - FILIPINO' --     25
+    , 'ASIAN - CAMBODIAN' --     17
+    , 'ASIAN - OTHER' --     17
+    , 'ASIAN - KOREAN' --     13
+    , 'ASIAN - JAPANESE' --      7
+    , 'ASIAN - THAI' --      4
+  ) THEN 'Asian'
+  ELSE 'Other' END AS race_group
+
 
 -- ICU stays
 FROM physionet-data.mimiciii_derived.icustay_detail
